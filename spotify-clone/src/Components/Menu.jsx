977cd62257fb571424de43bbe7473751a4e2115c
devdarji <you@example.com>
2022-08-28 18:08:32 +0530

    Left Menu Create

---
+++ spotify-clone/src/Components/Menu.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+
+function Menu(props) {
+  useEffect(() => {
+    const allLi = document.querySelector(".MenuContainer ul").querySelectorAll("li");
+
+    function changeMenuActive() {
+      allLi.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+    allLi.forEach((n) => n.addEventListener("click", changeMenuActive));
+  }, []);
+
+  return (
+    <div className="MenuContainer">
+      <p className="title">{props.title}</p>
+      <ul>
+        {props.menuObject &&
+          props.menuObject.map((menu) => (
+            <li key={menu.id}>
+              <a href="#">
+                <i>{menu.icon}</i>
+                <span>{menu.name}</span>
+              </a>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export { Menu };
